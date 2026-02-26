7aeec95e50c10fc81b1b480f97162cf4558e7b1e
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2026-02-26 14:45:16 +0100

    Dockerise server package
    (+fix start after build)

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -1,9 +1,11 @@
 import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+import { Address } from "./entities/Address";
 
 const datasource = new DataSource({
   type: "better-sqlite3",
   database: "./db.sqlite",
-  entities: ["./src/entities/**/*.{js,ts}"],
+  entities: [User, Address],
   logging: true,
   synchronize: true,
 });
